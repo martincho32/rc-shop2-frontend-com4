b9e44ed537ec66191b917a8439722df50d6e73d7
giomanconi <giomanconi@hotmail.com>
2020-01-25 17:25:29 -0300

    Bootstrap added | cards container component created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import logo from './images/react-logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
 
 function App() {
---
+++ src/components/CardsContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
+export function CardsContainer() {
+    return <h1>Hello</h1>;
+  }
