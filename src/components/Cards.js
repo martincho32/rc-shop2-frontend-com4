81f2be90b4dff442bff9092d3f5ef7ec52386678
giomanconi <giomanconi@hotmail.com>
2020-02-03 16:29:23 -0300

    Added 'alt' to cards img

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Cards(props) {
     return (
         <div className="card products-cards m-2 shadow">
-            <img src={props.images.path} className="card-img-top shadow-sm"></img>
+            <img src={props.images.path} className="card-img-top shadow-sm" alt={props.images.title}></img>
             <div className="card-body">
                 <h4 className="card-title price-text">$ {props.price} </h4>
                 <h6 className="font-italic text-success border-bottom pb-2"> {props.discountPercentage} </h6>
