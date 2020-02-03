48b024307401ec96640482a04951cbf9b37a0d5e
giomanconi <giomanconi@hotmail.com>
2020-02-03 15:54:26 -0300

    Home Page file name changed to HomePage.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/Home-page';
+import HomePage from './pages/HomePage';
 import Navbar from './components/Nabvar';
 import {
   Switch,
