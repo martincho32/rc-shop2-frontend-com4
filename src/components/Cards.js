ff4fb0834cd32cfe158ce7936c1968b2977997fe
giomanconi <giomanconi@hotmail.com>
2020-02-03 15:52:19 -0300

    forced checkout

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Price from 'price';
 
-export default function Cards (props) {
+export default function Cards(props) {
     return (
         <div className="card products-cards m-2 shadow">
-            <img src={props.img} className="card-img-top shadow-sm" alt={props.title}></img>
+            <img src={props.images.path} className="card-img-top shadow-sm"></img>
             <div className="card-body">
                 <h4 className="card-title price-text">$ {props.price} </h4>
-                <Price />
+                <h6 className="font-italic text-success border-bottom pb-2"> {"20%"} </h6>
                 <p className="card-text font-weight-light text-truncate "> {props.title + " - " + props.description} </p>
                 <div className="card-block text-right shop-btn">
                     <button className=" btn btn-success">Buy</button>
