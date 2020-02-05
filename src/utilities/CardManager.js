9b2b98902942eb3f0f73f18f1754e99470a57a2f
giomanconi <giomanconi@hotmail.com>
2020-02-05 18:26:13 -0300

    code cleanup

--- src/utilities/CardManager.js
+++ src/utilities/CardManager.js
@@ -1,38 +1,38 @@
 class CardManager {
 
     static key = "cartItems"
-    
+
     //Add one or more items to local storage
     static addProduct(prodID) {
         const products = [];
         if (getItem(CardManager.key)) {
             products = getItem(CardManager.key);
         }
-        products.push({ 'productID': prodID});
+        products.push({ 'productID': prodID });
         setItem([products]);
     }
 
     //remove a specific item from local storage
-    static removeProduct(prodID){
+    static removeProduct(prodID) {
         let storageProducts = getItem(CardManager.key);
-        let products = storageProducts.filter(p => p.productID !== prodID );
+        let products = storageProducts.filter(p => p.productID !== prodID);
         setItem([products]);
     }
-    
+
     //get local storage items
-    static getItem () {
+    static getItem() {
         let item = JSON.parse(localStorage.getItem(CardManager.key));
         return item;
     }
-    
+
     //set local storage items
-    static setItem (value) {
+    static setItem(value) {
         let item = JSON.stringify(value);
         localStorage.setItem(CardManager.key, item);
     }
 
     //clears local storage
-    static clearCart () {
+    static clearCart() {
         localStorage.removeItem(CardManager.key);
     }
 
