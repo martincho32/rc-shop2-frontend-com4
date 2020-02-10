e035b7eae833d004e774053920d435f0c50a6e2a
Martín Alejandro Barros <martinbarros86@gmail.com>
2020-02-10 12:51:24 -0300

    Show product detail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import logo from './images/react-logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage';
+import Navbar from './components/Nabvar';
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+import PageDetail from './Pagedetail';
 
 function App() {
   return (
     <div>
-      <img src={logo} className="image" alt="sample-resource" />
-      <h1 style={{ display: "inline" }}>Hello World!</h1>
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/pagedetail/:id" component={PageDetail} />
+        {/* <Route path="/Details" component={Details} /> */}
+        {/* <Route path="/Cart" component={Cart} /> */}
+        {/* <Route component={Default} /> */}
+      </Switch>
     </div>
   );
 }
