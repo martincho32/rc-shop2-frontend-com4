d694b5d14385b8d0b4eb0c1f922a3bf4fe38833c
giomanconi <giomanconi@hotmail.com>
2020-02-11 19:04:50 -0300

    productlist.js code cleanup

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 export default function Cards(props) {
     return (
-        <div className="card products-cards m-2 shadow">
+        <div className="card products-cards mb-4 ml-3 shadow">
             <Link to={`/pagedetail/${props._id}`} >
-                <img src={props.images[0].path} className="card-img-top shadow-sm" alt={props.images[0].title}
-                />
-            </Link>
-            <div className="card-body">
-                <h4 className="card-title price-text">$ {props.price} </h4>
-                <h6 className="font-italic text-success border-bottom pb-2"> {props.discountPercentage} </h6>
-                <p className="card-text font-weight-light text-truncate"> {props.model + " - " + props.brand} </p>
-                <div className="card-block text-right shop-btn">
-                    <button className=" btn btn-success px-3">Buy</button>
+                <img src={props.images[0].path} className="card-img-top shadow-sm" alt={props.images[0].title} />
+                <div className="card-body">
+                    <h4 className="card-title price-text">$ {props.price} </h4>
+                    <h6 className="font-italic text-success border-bottom pb-2"> {props.discountPercentage} </h6>
+                    <p className="card-text font-weight-light text-truncate"> {props.model + " - " + props.brand} </p>
                 </div>
-            </div>
+            </Link>
         </div>
+
     );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,34 +2,34 @@ import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import Cards from './Cards';
 
+
+
 export default function ProductList() {
     const [dbProducts, setDbProducts] = useState();
-
-    //Hook version of componentDidMount()
-    useEffect(() => {
-        async function getProducts() {
-            try {
-                const res = await Axios.get('/api/products');
-                setDbProducts(res.data)
-            } catch (e) {
-                console.error(e);
-            }
-        }
-
-        getProducts();
-    }, []);
-
     const products = dbProducts;
     let productsWithMap = [];
 
-    //checks if its not null or undefined
+    async function getProducts() {
+        try {
+            const res = await Axios.get('/api/products');
+            setDbProducts(res.data)
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    //Hook version of componentDidMount()
+    useEffect(() => {
+        getProducts();
+    }, []);
+
     if (products) {
         productsWithMap = products.map(p => <Cards key={p.id} {...p} />)
     }
 
     return (
         <>
-            <div className="container d-flex flex-wrap">
+            <div className="container d-flex flex-wrap p-2">
                 {productsWithMap}
             </div>
         </>
