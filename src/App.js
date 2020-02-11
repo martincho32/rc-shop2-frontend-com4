2a8c49928ed6bb03aead6d09ca25d4fe4b104f6c
Luis <luis.vazquez.r25@gmail.com>
2020-02-10 21:38:07 -0300

    start with task

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/HomePage';
-/* import Navbar from './components/Nabvar';
-import Carrousel from './components/Carrousel';
- *//* import ProductList from './components/ProductList'; */
-//import Footer from './components/Footer';
+//import Carrousel from './components/Carrousel';
+ /* import ProductList from './components/ProductList'; */
+import Footer from './components/Footer';
 import Cart from './components/Cart';
 import {
   Switch,
   Route
 } from "react-router-dom";
 import PageDetail from './Pagedetail';
+import Navbar from './components/Nabvar';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/pagedetail/:id" component={PageDetail} />
@@ -22,6 +23,7 @@ function App() {
         <Route path="/Cart" component={Cart} />
         {/* <Route component={Default} /> */}
       </Switch>
+      <Footer/>
     </div>
   );
 }
