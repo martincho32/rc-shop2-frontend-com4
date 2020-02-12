554c4b1382eee2686eeab18f7b8a0acabca5e909
Luis <luis.vazquez.r25@gmail.com>
2020-02-12 18:46:28 -0300

    acomode par de cosas

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ProductList from '../components/ProductList'; 
 import Carrousel from '../components/Carrousel';
 import Footer from '../components/Footer';
+import Nabvar from '../components/Nabvar';
 
 export default function HomePage() {
     return (
         <>
+         <Nabvar/>
          <Carrousel />
          <ProductList/>
          <Footer />  
