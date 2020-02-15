2f493ba233e475d7af16e4fa43a5052b0f2b04be
Luis <luis.vazquez.r25@gmail.com>
2020-02-14 21:06:51 -0300

    conflict solve2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import PageDetail from './Pagedetail';
+/* import PageDetail from './Pagedetail'; */
 import Section from './pages/Section';
 
 function App() {
@@ -21,7 +21,7 @@ function App() {
       <Nabvar />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/pagedetail/:id" component={PageDetail} />
+        {/* <Route exact path="/pagedetail/:id" component={PageDetail} /> */}
         <Route exact path="/section/:category" component={Section} />
         {/* <Route path="/Details" component={Details} /> */}
         <Route path="/Cart" component={Cart} />
